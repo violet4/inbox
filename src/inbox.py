0d8514e4f1a247635b3eb583f7db36c13f2e3346
Violet <7075901+violet4@users.noreply.github.com>
2022-08-08 14:02:28 -0700

    Update inbox.py

--- src/inbox.py
+++ src/inbox.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python3
+"""
+spoons
+minutes
+location
+prerequisites?
+notes
 
+
+workflow for helping figure out info about task..
+
+sometimes we just want.. inbox items that have no info so we can quickly enter many and check them later..
+sqlite necessary?.. probably..
+
+
+
+"""
